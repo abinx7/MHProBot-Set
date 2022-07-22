b0e48915f7f810909364ff717fe5ff1081cc84f8
SHAN-TG <85439835+SHAN-TG@users.noreply.github.com>
2022-07-22 12:13:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -114,7 +114,7 @@ async def next_page(bot, query):
             )
     btn.insert(0,
             [
-                InlineKeyboardButton(f"⚠️ Iɴғᴏ ⚠️",callback_data="moviess"),
+                InlineKeyboardButton(f"⚠️ Iɴғᴏ ⚠️",callback_data="seriess"),
                 InlineKeyboardButton(text=f"Cʜᴇᴄᴋ PM!", url=f"https://t.me/{temp.U_NAME}"),
                 InlineKeyboardButton("🗄 ғɪʟᴇ𝐬:", url="https://t.me/MovieHub_OTT")
             ])
@@ -942,7 +942,7 @@ async def auto_filter(client, msg, spoll=False):
         )
 
     btn.insert(0, [
-        InlineKeyboardButton(f"⚠️ Iɴғᴏ ⚠️",callback_data="moviess"),
+        InlineKeyboardButton(f"⚠️ Iɴғᴏ ⚠️",callback_data="seriess"),
         InlineKeyboardButton("🗄 ᴛᴏᴛᴀʟ ғɪʟᴇ𝐬:", url="https://t.me/MovieHub_OTT")
     ])
     btn.insert(0, [
